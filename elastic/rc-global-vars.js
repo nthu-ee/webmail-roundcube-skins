325ea9e2235e649b8174d7142bfc1ffdb2d020dc
Jack Cherng <jfcherng@gmail.com>
2019-07-03 02:43:17 +0800

    [Elastic] Use "global" rather than "window"
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- elastic/rc-global-vars.js
+++ elastic/rc-global-vars.js
@@ -1,4 +1,4 @@
 // this file is used to inject global/window variables for browserify
-bw = window.bw;
-rcmail = window.rcmail;
-rcube_webmail = window.rcube_webmail;
+bw = global.bw;
+rcmail = global.rcmail;
+rcube_webmail = global.rcube_webmail;
