1f4e0c932cedfaff9a558e2df2799ac287d0ba85
Jack Cherng <jfcherng@gmail.com>
2019-09-24 16:42:04 +0800

    Auto show mail selection checkboxes if in touch mode
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- elastic/ui.export.js
+++ elastic/ui.export.js
@@ -3,4 +3,9 @@ global.UI = UI;
 $(() => {
   $('[data-toggle="tooltip"]').tooltip({ container: 'body', trigger: 'hover' });
   $('[data-toggle="popover"]').popover({ container: 'body', trigger: 'hover' });
+
+  // if the user uses touch device, show "mail selection" checkboxes automatically
+  if ($('html').hasClass('touch')) {
+    $('#messagelist').addClass('withselection');
+  }
 });
